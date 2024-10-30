f079e17f07b41a15b64d262542fcc8a4422b604d
Rafael Massimo <rafael_massimo@hotmail.com>
2024-10-29 23:48:01 -0700

    start to implement sessionStorage

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,6 +1,5 @@
-
-import { create, StateCreator } from 'zustand';
-import { persist, } from 'zustand/middleware';
+import { create, type StateCreator } from 'zustand';
+import { createJSONStorage, persist, StateStorage } from 'zustand/middleware';
 
 interface PersonState {
 	firstName: String;
@@ -13,16 +12,31 @@ interface Actions {
 }
 
 const storeAPI: StateCreator<PersonState & Actions> = (set) => ({
-firstName: '',
-lastName: '',
+	firstName: '',
+	lastName: '',
 
-setFirstName: (value: string) => set((state) => ({ firstName: value })),
-setLastName: (value: string) => set((state) => ({ lastName: value })),
-})
+	setFirstName: (value: string) => set((state) => ({ firstName: value })),
+	setLastName: (value: string) => set((state) => ({ lastName: value })),
+});
 
+const sessionStorage: StateStorage = {
+	getItem: function (name: string): string | null | Promise<string | null> {
+		console.log('get Item', name);
+
+		return null;
+	},
+	setItem: function (name: string, value: string): void | Promise<void> {
+		console.log('setItem', name, value);
+	},
+	removeItem: function (name: string): void | Promise<void> {
+		console.log('removeItem', name);
+	},
+};
 
 // In this store we are applying the middleware Persist note that we have remove the store from inside the create block and put outside to increase the readability
 export const userPersonStore = create<PersonState & Actions>()(
-    persist(storeAPI, {name: 'person-storage'})
-
+	persist(storeAPI, {
+		name: 'person-storage',
+		storage: createJSONStorage(() => sessionStorage),
+	}),
 );
