d9d191c5cd8ac47f10520bc345e21dd9ee4fc4bc
Rafael Massimo <rafael_massimo@hotmail.com>
2024-11-06 22:04:01 -0800

    start to create the tasks store

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -1,26 +1,28 @@
 import { JiraTasks } from '../../components';
+import { useTaskStore } from '../../stores';
+import { logger } from '../../stores/middlewares/logger.middleware';
 
 export const JiraPage = () => {
-  return (
-    <>
-      <h1>Tareas</h1>
-      <p>Manejo de estado con objectos de Zustand</p>
-      <hr />
-
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          
-          <JiraTasks title='Pendientes' value='pending' />
-          
-          <JiraTasks title='Avanzando' value='in-progress' />
-          
-          <JiraTasks title='Terminadas' value='done' />
-
-      </div>
-
-      
+	const tasks = useTaskStore((state) => state.tasks);
+	const openTasks = useTaskStore((state) => state.getTasksByStatus('open'));
+	const inProgressTasks = useTaskStore((state) => state.getTasksByStatus('in-progress'));
+	const doneTasks = useTaskStore((state) => state.getTasksByStatus('done'));
 
 
 
-    </>
-  );
+	return (
+		<>
+			<h1>Tareas</h1>
+			<p>Manejo de estado con objectos de Zustand</p>
+			<hr />
+
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+				<JiraTasks tasks={openTasks} title="Pendientes" value="open" />
+
+				<JiraTasks tasks={inProgressTasks}  title="Avanzando" value="in-progress" />
+
+				<JiraTasks tasks={doneTasks}  title="Terminadas" value="done" />
+			</div>
+		</>
+	);
 };
--- src/stores/tasks/task.store.ts
+++ src/stores/tasks/task.store.ts
@@ -1,7 +1,25 @@
 
-interface TaskState {
+import { create, StateCreator } from 'zustand';
+import { Task, TaskStatus } from '../../interfaces';
 
-    tasks:
+interface TaskState {
+	tasks: Record<string, Task>; //{[key: string]: Task}
+    getTasksByStatus: (status: TaskStatus) => Task[],
 }
 
+const storeApi: StateCreator<TaskState> = (set, get) => ({
+	tasks: {
+		'ABC-1': { id: 'ABC-1', title: 'Task 1', status: 'in-progress' },
+		'ABC-2': { id: 'ABC-2', title: 'Task 2', status: 'open' },
+		'ABC-3': { id: 'ABC-3', title: 'Task 3', status: 'in-progress' },
+		'ABC-4': { id: 'ABC-4', title: 'Task 4', status: 'open' },
+	},
 
+    getTasksByStatus: (status: TaskStatus) => {
+        const tasks = get().tasks;
+        return Object.values(tasks).filter(task => task.status === status )
+    }
+});
+
+//Here you can create the store but first is better to setup this store inside the storeApi
+export const useTaskStore = create<TaskState>()(storeApi)
